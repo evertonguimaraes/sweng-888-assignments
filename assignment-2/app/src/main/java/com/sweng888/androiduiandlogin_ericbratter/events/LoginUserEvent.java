898f4162b1c1250f11a966eacdb164ffffa0a304
Eric Bratter <eric.bratter@gmail.com>
2018-11-08 20:13:36 -0500

    add offline login from sqlite

--- assignment-2/app/src/main/java/com/sweng888/androiduiandlogin_ericbratter/events/LoginUserEvent.java
+++ assignment-2/app/src/main/java/com/sweng888/androiduiandlogin_ericbratter/events/LoginUserEvent.java
@@ -4,7 +4,9 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,6 +20,7 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
@@ -40,19 +43,24 @@ public class LoginUserEvent {
     public Single<User> execute(@NonNull final String email, @NonNull final String password) {
         mAuth = FirebaseAuth.getInstance();
 
-        return Observable.create(new ObservableOnSubscribe<FirebaseUser>(){
+        return Observable.create(new ObservableOnSubscribe<String>() {
             @Override
-            public void subscribe(final ObservableEmitter<FirebaseUser> emitter) throws Exception {
+            public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
                 mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.getException() != null && task.getException() instanceof FirebaseNetworkException){
+                                    emitter.onNext(email);
+                                    emitter.onComplete();
+                                    return;
+                                }
+
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "createUserWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    emitter.onNext(user);
-
+                                    emitter.onNext(user.getEmail());
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
@@ -62,10 +70,17 @@ public class LoginUserEvent {
                             }
                         });
             }
-        }).switchMap(new Function<FirebaseUser, Observable<User>>() {
+        }).switchMap(new Function<String, Observable<User>>() {
             @Override
-            public Observable<User> apply(FirebaseUser firebaseUser) throws Exception {
-                return repository.getById(firebaseUser.getEmail());
+            public Observable<User> apply(String email) throws Exception {
+                return repository.getById(email);
+            }
+        }).filter(new Predicate<User>() {
+            // Perform a local password verification as well
+            // this would cover the case of no internet
+            @Override
+            public boolean test(User user) throws Exception {
+                return user.getPassword().equals(password);
             }
         }).subscribeOn(Schedulers.io()).firstOrError().observeOn(AndroidSchedulers.mainThread());
     }
